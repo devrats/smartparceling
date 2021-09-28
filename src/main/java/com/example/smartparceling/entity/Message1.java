b44a661b93ed8b7ddb8e79779ca0c21c417584bf
Devrats <devvratsharma9759@gmail.com>
2021-09-28 20:33:43 +0530

    added chat options

---
+++ src/main/java/com/example/smartparceling/entity/Message1.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devvrat Sharma (devrats)
+ *   Date: 28-Sep-21
+ *   Time: 7:43 PM
+ *   File: Message1.java
+ */
+
+package com.example.smartparceling.entity;
+
+public class Message1 {
+}
